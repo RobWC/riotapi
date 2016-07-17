a4663a16f1d5d147ccf54b6ff24772266e986fe1
Rob Cameron <rwcameron@gmail.com>
2016-07-17 01:05:22 -0700

    update docs

---
+++ flymake_summonerdoc_test.go
@@ -0,0 +1,11 @@
+// Package riotapi riot REST API for League of Legends
+package riotapi
+
+// This is a golang implementation of the Riot API
+// More information about the Riot API can be found here: https://developer.riotgames.com
+
+// Rate Limiting
+// Using an API can often be a challenge in code. The Riot API client includes
+// friendly rate limiting. This preserves your applications access to the API.
+
+// Author: Rob Cameron
