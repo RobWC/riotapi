a1f02c89a929b47d2cdef13aa5cdbe63dff229f7
Rob Cameron <rwcameron@gmail.com>
2016-07-17 01:09:25 -0700

    update docs

--- doc.go
+++ doc.go
@@ -1,11 +1,9 @@
-// Package riotapi riot REST API for League of Legends
-package riotapi
-
+// Package riotapi - riot REST API for League of Legends
 // This is a golang implementation of the Riot API
 // More information about the Riot API can be found here: https://developer.riotgames.com
-
+//
 // Rate Limiting
 // Using an API can often be a challenge in code. The Riot API client includes
 // friendly rate limiting. This preserves your applications access to the API.
-
 // Author: Rob Cameron
+package riotapi
