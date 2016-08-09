0d734cfe3e89b8022f34090211b3865b409b7da8
Rob Cameron <rwcameron@gmail.com>
2016-08-08 22:43:34 -0700

    add scaffolding

---
+++ api_static_champion.go
@@ -0,0 +1,9 @@
+package riotapi
+
+// https://developer.riotgames.com/api/methods#!/1055/3633
+
+// StaticChampion get static champion data
+
+// https://developer.riotgames.com/api/methods#!/1055/3622
+
+// StaticChampionByID get a champion by ID
